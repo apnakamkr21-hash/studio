269411a60c7cfd1d076e73f8f3b4a21f24dfd9c7
Harshad Shewale <apnakamkr21@gmail.com>
2025-11-18 06:17:52 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1630940285-53922",
+  "appId": "1:871910965548:web:6bf4595b4c58d416afac89",
+  "apiKey": "AIzaSyDW1fYb2SRs_GsN7oasFfnTbV_Oc9k23vs",
+  "authDomain": "studio-1630940285-53922.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "871910965548"
+};
