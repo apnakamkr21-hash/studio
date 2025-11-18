15c1ff4c46733c594bde250a3a84c04494e189a9
Harshad Shewale <apnakamkr21@gmail.com>
2025-11-18 06:20:44 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Missing or insuffi

--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -1,13 +1,38 @@
 'use client';
+import { useUser } from '@/firebase';
 import { Header } from './header';
+import { Skeleton } from '../ui/skeleton';
 
 export function AppLayout({ children }: { children: React.ReactNode }) {
+  const { isUserLoading } = useUser();
+
   return (
     <div className="flex min-h-screen w-full flex-col">
       <Header />
       <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
-        {children}
+        {isUserLoading ? <AppSkeleton /> : children}
       </main>
     </div>
   );
 }
+
+const AppSkeleton = () => (
+  <div className="space-y-12">
+    <div className="space-y-4">
+      <Skeleton className="h-10 w-1/3" />
+      <Skeleton className="h-6 w-2/3" />
+    </div>
+    <div className="space-y-6">
+      <Skeleton className="h-8 w-1/4" />
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {[...Array(4)].map((_, i) => (
+          <div key={i} className="space-y-4">
+            <Skeleton className="h-48 w-full" />
+            <Skeleton className="h-6 w-5/6" />
+            <Skeleton className="h-4 w-1/2" />
+          </div>
+        ))}
+      </div>
+    </div>
+  </div>
+);
